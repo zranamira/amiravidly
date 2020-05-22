295978dcc3007b3de1369b55dc0ac107657c097f
amira <zranamira9@gmail.com>
2020-05-22 14:07:49 +0200

    another commit

--- vidly/settings.py
+++ vidly/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '=3uzue%*6!3xfhfe=^6d)&62diwelmy0@m8*(v#hy)l@$edq1f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['shrouded-oasis-61124.herokuapp.com']
+ALLOWED_HOSTS = ['vidly.herokuapp.com']
 
 # Application definition
 
